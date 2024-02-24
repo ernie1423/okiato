8ded976838c5acfe95f9e85bbac9f03b9b0622c7
ernie1423 <v.ulanocc@gmail.com>
2024-02-24 17:56:12 +0300

    add pixi.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Stage, Sprite } from "@pixi/react";
+import "./App.css"
+import CircleSVG from "./assets/circle.svg"
+
 const App = () => {
-  return <div>Hello, okiato!</div>;
+  return <Stage
+    width={window.innerWidth}
+    height={window.innerHeight}
+  >
+    <Sprite
+      image={CircleSVG}
+      x={window.innerWidth/2}
+      y={window.innerHeight/2}
+      anchor={{ x: 0.5, y: 0.5 }}
+    />
+  </Stage>;
 };
 
 export default App;
